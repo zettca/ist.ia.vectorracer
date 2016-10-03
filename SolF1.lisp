f6e4f24f53c3e35dd6b55cc1972e162d53e5ad8f
Bruno Henriques <corruption1337@gmail.com>
2016-10-03 17:55:54 +0000

    Implement alpha solution

--- SolF1.lisp
+++ SolF1.lisp
@@ -5,18 +5,19 @@
 (load "auxfuncs.lisp")
 
 (defun isObstaclep (pos track) 
-  "check if there is an obstacle at position pos of the track"
-  t)
+  (let ((l (nth 0 pos)) (c (nth 1 pos)) (env (track-env track)))
+    (not (nth c (nth l env)))))
 
-(defun isGoalp (st) 
-  "check if st is a goal state"
-  t)
+(defun isGoalp (st)
+  (let ((pos (state-pos st)) (ends (track-endpositions (state-track st))))
+    (loop for p in ends do
+      (when (equal pos p) (return-from isGoalp t)))))
 
 (defun nextState (st act)
-  "generate the nextState after state st and action act"
-  (make-STATE :POS '(3 16)
-	      :VEL '(1 3)
-	      :ACTION act
-	      :COST -100))
-
-
+  (let ((pos (state-pos st)) (vel (state-vel st)) (cost 1))
+    (setq newVel (list (+ (nth 0 vel) (nth 0 act)) (+ (nth 1 vel) (nth 1 act))))
+    (setq newPos (list (+ (nth 0 pos) (nth 0 newVel)) (+ (nth 1 pos) (nth 1 newVel))))
+    (when (isObstaclep newPos (state-track st))
+      (setq newPos pos) (setq newVel '(0 0)) (setq cost 20))
+    (when (isGoalp (make-state :POS newPos :TRACK (state-track st))) (setq cost -100)) ; ??
+    (make-STATE :POS newPos :VEL newVel :ACTION act :COST cost :TRACK (state-track st))))
