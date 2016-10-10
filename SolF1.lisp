c42d0578dc2ccab3b030ac629ad41703f4071f13
Bruno Henriques <corruption1337@gmail.com>
2016-10-10 20:30:40 +0000

    Reduce let usage Change to setq multi-variable assignment

--- SolF1.lisp
+++ SolF1.lisp
@@ -1,23 +1,21 @@
-
-;;; These functions, and any other ones needed must be implemented
+;;; IA 2016/2017 tg046
+;;; 78013 Bruno Henriques
+;;; 82094 Leonardo Vieira
 
 (load "datastructures.lisp")
 (load "auxfuncs.lisp")
 
-(defun isObstaclep (pos track) 
-  (let ((l (nth 0 pos)) (c (nth 1 pos)) (env (track-env track)))
-    (not (nth c (nth l env)))))
+(defun isObstaclep (pos track)
+    (not (nth (second pos) (nth (first pos) (track-env track)))))
 
 (defun isGoalp (st)
-  (let ((pos (state-pos st)) (ends (track-endpositions (state-track st))))
-    (loop for p in ends do
-      (when (equal pos p) (return-from isGoalp t)))))
+  (loop for p in (track-endpositions (state-track st)) do
+    (when (equal (state-pos st) p) (return-from isGoalp t))))
 
 (defun nextState (st act)
-  (let ((pos (state-pos st)) (vel (state-vel st)) (cost 1))
-    (setq newVel (list (+ (nth 0 vel) (nth 0 act)) (+ (nth 1 vel) (nth 1 act))))
-    (setq newPos (list (+ (nth 0 pos) (nth 0 newVel)) (+ (nth 1 pos) (nth 1 newVel))))
-    (when (isObstaclep newPos (state-track st))
-      (setq newPos pos) (setq newVel '(0 0)) (setq cost 20))
-    (when (isGoalp (make-state :POS newPos :TRACK (state-track st))) (setq cost -100)) ; ??
+  (let* ((pos (state-pos st)) (vel (state-vel st)) (cost 1)
+    (newVel (list (+ (nth 0 vel) (nth 0 act)) (+ (nth 1 vel) (nth 1 act))))
+    (newPos (list (+ (nth 0 pos) (nth 0 newVel)) (+ (nth 1 pos) (nth 1 newVel)))))
+    (when (isObstaclep newPos (state-track st)) (setq newPos pos newVel '(0 0) cost 20))
+    (when (isGoalp (make-state :POS newPos :TRACK (state-track st))) (setq cost -100))
     (make-STATE :POS newPos :VEL newVel :ACTION act :COST cost :TRACK (state-track st))))
